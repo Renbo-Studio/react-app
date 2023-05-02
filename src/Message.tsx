201a2cf180fe8c3c207d9a6fefd21c20d03fbf9b
Joshua Umahi <98918202+Renbo-Studio@users.noreply.github.com>
2023-05-02 23:12:13 +0100

    Getting Started

---
+++ src/Message.tsx
@@ -0,0 +1,11 @@
+//** Pascal Casing **//
+function Message()
+{
+    //~~ JSX: JavaScript XML
+    const name = "Doi";
+    if(name)
+        return <h1>Hi MOM it is {name}</h1>;
+    return <h1>Hi MOM</h1>;
+}
+
+export default Message;
